dd2265e93616364ed05eea8d7073ab4866aa9547
Nathan <nlapinski@spotxchange.com>
2016-09-12 18:37:24 -0600

    stword :: updates to test page
    adding buttons for sns login

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');    // used for parsing incoming HTTP requests
 
+
+// for serving static files
+app.use(express.static('public'));
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 var port = process.env.PORT || 8080;        // set our port
 
